a134081a98ed8c984db02b614f45a0b2fd89fc3c
Armando Gonzalez <armangonzal@gmail.com>
2021-09-16 10:45:12 -0500

    Update service layer with comments for implementation

--- capstone/src/main/java/com/mthree/capstone/service/CapstoneServiceImpl.java
+++ capstone/src/main/java/com/mthree/capstone/service/CapstoneServiceImpl.java
@@ -31,36 +31,82 @@ public class CapstoneServiceImpl implements CapstoneService {
     
     @Override
     public User authorLogin(String username, String password) {
+        /*
+        Author author = this.dao.getAuthor(username);
+        Author guest = this.dao.getGuest();
+        if(password = author.getPassword() ) return author;
+        else return guest;
+        */
         return new User("no name");
     }
 
     @Override
     public User getAuthorById(int author_id) {
+        /*
+        return this.dao.getAuthor(author_id);
+        */
         return new User("no name");
     }
 
     @Override
     public boolean addAuthor(User author) {
+        /*
+        try {
+            this.dao.addAuthor(author);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+        */
         return false;
     }
 
     @Override
     public BlogPost getBlogPostById(int post_id) {
+        /*
+        try {
+            return this.dao.getBlogPost(post_id);
+        } catch (Exception e) {
+            throw new Exception("no blog post");
+        }
+        */
         return new BlogPost("Title");
     }
 
     @Override
     public List<BlogPost> getAllBlogPosts() {
+        /*
+        try {
+            return this.dao.getAllBlogPosts();
+        } catch (Exception e) {
+            throw new Exception("blog access error");
+        }
+        */
         return new ArrayList<BlogPost>();
     }
 
     @Override
     public List<BlogPost> getBlogPostsByHastag(String hashTag) {
+        /*
+        try {
+            return this.dao.getAllBlogPostsByHashtag(hashTag);
+        } catch (Exception e) {
+            throw new Exception("blog access error");
+        }
+        */
         return new ArrayList<BlogPost>();
     }
 
     @Override
     public boolean addBlogPost(BlogPost blogPost) {
+        /*
+        try {
+            this.dao.addBlogPost(blogPost);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+        */
         return false;
     }
 
