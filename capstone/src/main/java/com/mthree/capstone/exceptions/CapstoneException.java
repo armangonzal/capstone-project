5535a181bb23cb840b284f7935fe410600ebfe5b
ProfessorMewtwo <70973995+ProfessorMewtwo@users.noreply.github.com>
2021-09-16 13:28:00 -0500

    Add files via upload

--- capstone/src/main/java/com/mthree/capstone/exceptions/CapstoneException.java
+++ capstone/src/main/java/com/mthree/capstone/exceptions/CapstoneException.java
@@ -3,12 +3,36 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mthree.capstone.exceptions;
+package capstone.src.main.java.com.mthree.capstone.exceptions;
 
 /**
  *
  * @author ArmandoGonzalez
  */
-public class CapstoneException {
-    
+public class CapstoneException  {
+	
+	//This one was a test exception
+	public class DataAccessException extends Exception { 
+	    public DataAccessException(String search, String catagorey) {
+	        super(search + " is not an available " + catagorey + ". Data cannot be retrieved.");
+	    }
+	}
+	
+	public class NoSuchBlogPostException extends Exception { 
+	    public NoSuchBlogPostException(String search) {
+	        super(search + " is not a Blog Post. Data cannot be retrieved.");
+	    }
+	}
+	
+	public class NoSuchAuthorException extends Exception { 
+	    public NoSuchAuthorException(String search) {
+	        super(search + " is not an Author. Data cannot be retrieved.");
+	    }
+	}
+	
+	public class InvalidLoginException extends Exception {
+		public InvalidLoginException() {
+			super("Invalid credentials. Cannot be logged in.");
+		}
+	}
 }
