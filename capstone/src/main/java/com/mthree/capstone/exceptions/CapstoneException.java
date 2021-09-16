cd423f1e0722c7e78781c3b77889d0b89d7fffab
Armando Gonzalez <armangonzal@gmail.com>
2021-09-16 13:37:03 -0500

    fix import package

--- capstone/src/main/java/com/mthree/capstone/exceptions/CapstoneException.java
+++ capstone/src/main/java/com/mthree/capstone/exceptions/CapstoneException.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package capstone.src.main.java.com.mthree.capstone.exceptions;
+package com.mthree.capstone.exceptions;
 
 /**
  *
