b79db224a240c877d23df5bf345f0f03caea368f
Armando Gonzalez <armangonzal@gmail.com>
2021-09-16 13:05:25 -0500

    create Dao interface

--- capstone/src/main/java/com/mthree/capstone/dao/Dao.java
+++ capstone/src/main/java/com/mthree/capstone/dao/Dao.java
@@ -5,10 +5,25 @@
  */
 package com.mthree.capstone.dao;
 
+import com.mthree.capstone.dto.*;
+import java.util.List;
+
 /**
  *
  * @author ArmandoGonzalez
  */
 public interface Dao {
+    User getAuthor(String username); //throws NoSuchAuthorException
     
+    User getAuthor(int author_id); //throws NoSuchAuthorException
+    
+    User addAuthor(User author); //throws DataAccessException
+    
+    BlogPost getBlogPost(int post_id); //throws NoSuchBlogPostException
+    
+    List<BlogPost> getAllBlogPosts();
+    
+    List<BlogPost> getAllBlogPostsByHashtag(String hashTag);
+    
+    BlogPost addBlogPost(BlogPost blogPost); //throws DataAccessException
 }
--- capstone/src/main/java/com/mthree/capstone/dao/DaoImpl.java
+++ capstone/src/main/java/com/mthree/capstone/dao/DaoImpl.java
@@ -5,10 +5,67 @@
  */
 package com.mthree.capstone.dao;
 
+import com.mthree.capstone.dto.BlogPost;
+import com.mthree.capstone.dto.User;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+/*
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.stereotype.Repository;
+*/
 /**
  *
  * @author 88bry
  */
-public class DaoImpl {
+
+//@Repository
+//@Profile({"database", "prod"})
+public class DaoImpl implements Dao{
+    
+    //private final JdbcTemplate jdbcTemplate;
+/*
+    @Autowired
+    public DaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    */
+    @Override
+    public User getAuthor(String username) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public User getAuthor(int author_id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public User addAuthor(User author) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public BlogPost getBlogPost(int post_id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<BlogPost> getAllBlogPosts() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<BlogPost> getAllBlogPostsByHashtag(String hashTag) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public BlogPost addBlogPost(BlogPost blogPost) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
