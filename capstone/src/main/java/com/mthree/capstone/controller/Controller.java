6c44c156084bf32090b3c7cfa31a6b7b17addc02
BlueJWay <brendan_flood@yahoo.com>
2021-09-17 09:26:36 -0500

    Edits to properly match endpoints and incorporate Exceptions that the service layer functions throw

--- capstone/src/main/java/com/mthree/capstone/controller/Controller.java
+++ capstone/src/main/java/com/mthree/capstone/controller/Controller.java
@@ -7,6 +7,7 @@ package com.mthree.capstone.controller;
 
 import com.mthree.capstone.service.*;
 import com.mthree.capstone.dto.*;
+import com.mthree.capstone.exceptions.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -31,7 +32,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  *
  * @author ArmandoGonzalez
  */
-public class Controller throws InvalidLoginException{
+public class Controller throws InvalidLoginException, NoSuchBlogPostException, NoSuchAuthorException, DataAccessException{
     
     CapstoneService service;
     Author currentAuthor;
@@ -41,57 +42,56 @@ public class Controller throws InvalidLoginException{
         this.service = service;
     }
 
-    @PostMapping("home") //only html page in repo is "index" but home seems like a fitting placeholder
+    @PostMapping("login") //only html page in repo is "index" but home seems like a fitting placeholder
     public String loginToAuthor(HttpServletRequest request) throws InvalidLoginException{
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         currentAuthor = service.authorLogin(username, password);
-        return "home";
+        return "login";
         
-    }
-
-    @GetMapping("home")
-    public String homePage(Model model) {
-        
-        // Teachers
-        List<Teacher> teacherList = service.getRecentTeachers();
-        model.addAttribute("teachers", teacherList);
-        
-        // BlogPosts
-        List<BlogPost> blogPostList = service.getAllBlogPost();
-        model.addAttribute("blogPosts", blogPostList);
-        
-        // BlogPost by name
-        Map<Integer, String> blogPostsByName = blogPostList.stream().collect(
-                Collectors.toMap(BlogPost::getBlogPostId, BlogPost::getBlogPostName));
-        
-        model.addAttribute("blogPostsById", blogPostsByName);
-        
-        // Locations
-        List<Location> locationList = service.getAllLocations();
-        model.addAttribute("locations", locationList);
-        
-        // Location by name
-        Map<Integer, String> locationsByName = locationList.stream().collect(
-                Collectors.toMap(Location::getLocationId, Location::getLocationName));
-        
-        model.addAttribute("locationsById", locationsByName);
-        
-        return "home";
     }
 
     
 
+    
 
-    @GetMapping("blogPosts")
-    public String displayblogPosts(Model model) {
-        List<blogPosts> = service.getAllBlogPosts();
+
+    @GetMapping("blog")
+    public String displayblogPosts(Model model) throws DataAccessException{
+        List<BlogPost> blogPosts = service.getAllBlogPosts();
         model.addAttribute("blogPosts", blogPosts);
         return "blogPosts";
     }
 
-    @PostMapping("addBlogPost")
-    public String addBlogPost(int postId, int authorId, String title, String body, LocalDate expiration) {
+
+    @GetMapping("blog")
+    public String displayblogPostsByHashtag(HttpServletRequest request, Model model) throws NoSuchBlogPostException{
+        String hashtag = request.getParameter("hashtag")
+        List<BlogPost> blogPosts = service.getlogPostsByHashtag(hashtag);
+        model.addAttribute("blogPosts", blogPosts);
+        return "blogPosts";
+    }
+
+    @GetMapping("author")
+    public String getAuthorById(HttpServletRequest request, Model model) throws NoSuchAuthorException{
+        int authorId = Integer.parseInt(request.getParameter("author_id"));
+        Author author = service.getAuthoryId(authorId);
+        model.addAttribute("author", author);
+        return "blogPosts";
+    }
+
+    @PostMapping("author")
+    public String addBlogPost(HttpServletRequest request) throws DataAccessException{
+        int postId = Integer.parseInt(request.getParameter("postId"));
+
+        int authorId = Integer.parseInt(request.getParameter("authorId"));
+
+        String title = request.getParameter("title");
+
+        String body = request.getParameter("textBody");
+
+        LocalDate expiration = LocalDate.parse(request.getParameter("firstName"));
+
         BlogPost blogPost = new BlogPost();
         blogPost.setPostId(postId);
         blogPost.setAuthorId(authorId);
@@ -99,33 +99,29 @@ public class Controller throws InvalidLoginException{
         blogPost.setTextBody(body);
         blogPost.setDateCreated(LocalDate.now());
         blogPost.setExpiration(expiration);
-        CapstoneService.addBlogPost(blogPost);
-        
-        return "redirect:/blogPosts";
-    }
-
-    @GetMapping("editBlogPost")
-    public String editBlogPost(Integer id, Model model) {
-        BlogPost blogPost = blogPostService.getBlogPostById(id);
-        model.addAttribute("blogPost", blogPost);
-        return "editBlogPost";
-    }
-
-    @PostMapping("editBlogPost")
-    public String performEditBlogPost(HttpServletRequest request) {
-        int id = Integer.parseInt(request.getParameter("id"));
-        BlogPost blogPost = service.getBlogPostById(id);
-        
-        blogPost.setPostId(request.getParameter("postId"));
-        blogPost.setAuthorIde(request.getParameter("authorId"));
-        blogPost.setTextBody(request.getParameter("textBody"));
-        blogPost.setTitle(request.getParameter("title"));
-        blogPost.setDateCreated(request.getParameter("dateCreated"));
-        blogPost.setExpiration(request.getParameter("expiration"));
-        
         service.addBlogPost(blogPost);
         
-        return "redirect:/blogPosts";
+        return "author";
+    }
+
+    @PostMapping("register")
+    public String addAuthor(HttpServletRequest request) throws DataAccessException{
+
+        int authorId = Integer.parseInt(request.getParameter("authorId"));
+
+        String firstName = request.getParameter("firstName");
+
+        String lastName = request.getParameter("lastName");
+
+        
+        Author author = new Author();
+        author.setAuthorId(authorId);
+        author.setFirstName(firstName);
+        author.setlastName(lastName);
+        author.setIsAdmin(false);
+        service.addAuthor(author);
+        
+        return "author";
     }
 
 
